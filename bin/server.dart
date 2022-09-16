d7863c17de86c95137a652690187029bb35175d0
lindeer <le.chang118@gmail.com>
2022-09-16 15:59:31 +0800

    allow server support CORS

--- bin/server.dart
+++ bin/server.dart
@@ -1,24 +1,20 @@
 
 import 'package:cloudoc/server/api.dart' as api;
-import 'package:cloudoc/server/view.dart' as view;
-import 'package:shelf/shelf.dart' show Cascade, Pipeline, logRequests;
+import 'package:shelf/shelf.dart' show Pipeline, logRequests;
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 const _dataRoot = 'test/_test_';
 const _defaultPort = 8989;
 
 void main(List<String> args) {
-  final webRoot = args.isNotEmpty ? args[0] : 'build/web';
-  final app = Cascade()
-      .add(view.serve(webRoot))
-      .add(api.serve(_dataRoot))
-      .handler;
 
   final handler = const Pipeline()
       .addMiddleware(logRequests())
-      .addHandler(app);
+      .addMiddleware(corsHeaders())
+      .addHandler(api.serve(_dataRoot));
 
-  final port = args.length > 1 ? (int.tryParse(args[1]) ?? _defaultPort) : _defaultPort;
+  final port = args.isNotEmpty ? (int.tryParse(args[0]) ?? _defaultPort) : _defaultPort;
   io.serve(handler, '0.0.0.0', port).then((server) {
     print('start server at ${server.address.host}:${server.port} ...');
   });
--- lib/server/view.dart
+++
@@ -1,4 +0,0 @@
-import 'package:shelf/shelf.dart' show Handler;
-import 'package:shelf_static/shelf_static.dart';
-
-Handler serve(String dir) => createStaticHandler(dir, defaultDocument: 'index.html');
