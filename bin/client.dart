94f9befc1be5475658dd37b8fedb6fb2dad3e608
lindeer <le.chang118@gmail.com>
2022-09-16 10:49:34 +0800

    client request

--- bin/client.dart
+++ bin/client.dart
@@ -1,3 +1,4 @@
+import 'package:cloudoc/client/service.dart';
 import 'package:cloudoc/file_entity.dart';
 import 'package:flutter/material.dart';
 
@@ -31,6 +32,24 @@ class _BrowserPage extends StatefulWidget {
 class _BrowserPageState extends State<_BrowserPage> {
   final _pathStack = <String>[];
   final _entities = <FileEntity>[];
+  final _title = ValueNotifier('');
+  final _service = Service("0.0.0.0:8989");
+
+  @override
+  void initState() {
+    super.initState();
+
+    _enterFolder('desktop');
+  }
+
+  void _enterFolder(String entry) async {
+    _pathStack.add(entry);
+    _title.value = '/${_pathStack.join('/')}';
+    final entities = await _service.listEntities(_title.value);
+    _entities..clear()..addAll(entities);
+    setState(() {
+    });
+  }
 
   void _onNewMenu() {
   }
@@ -45,7 +64,12 @@ class _BrowserPageState extends State<_BrowserPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_pathStack.join('/')),
+        title: ValueListenableBuilder<String>(
+          valueListenable: _title,
+          builder: (ctx, value, _) {
+            return Text(value);
+          },
+        ),
       ),
       body: Center(
         child: FractionallySizedBox(
---
+++ lib/client/service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert' show json;
+import 'dart:io' show HttpStatus;
+
+import 'package:http/http.dart' as http;
+
+import '../file_entity.dart';
+
+class Service {
+  final String authority;
+
+  Service(this.authority);
+
+  Future<List<FileEntity>> listEntities(String path) async {
+    final res = await http.get(Uri.http(authority, path));
+    if (res.statusCode == HttpStatus.ok) {
+      final body = json.decode(res.body) as Map<String, dynamic>;
+      final list = body['data'] as List<dynamic>;
+      return list.map((e) {
+        final map = e as Map<String, dynamic>;
+        return FileEntity.fromJson(map);
+      }).toList(growable: false);
+    } else {
+      throw Exception("Failed to fetch entities from '$path'");
+    }
+  }
+}
