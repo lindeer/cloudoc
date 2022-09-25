29b2ea3643e2e2cd6b890ec197e6d8a036e2cb04
lindeer <le.chang118@gmail.com>
2022-09-25 21:42:59 +0800

    port and dir params of server

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -4,17 +4,32 @@ import 'package:shelf/shelf.dart' show Pipeline, logRequests;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
-const _dataRoot = 'test/_test_';
+const _dataRoot = '_data';
 const _defaultPort = 8989;
 
 void main(List<String> args) {
+  String? dir;
+  String? p;
+  final it = args.iterator;
+  while (it.moveNext()) {
+    final opt = it.current;
+    switch (opt) {
+      case '-p':
+      case '--port':
+        it.moveNext();
+        p = it.current;
+        break;
+      default:
+        dir = opt;
+    }
+  }
 
   final handler = const Pipeline()
       .addMiddleware(logRequests())
       .addMiddleware(corsHeaders())
-      .addHandler(api.serve(_dataRoot));
+      .addHandler(api.serve(dir ?? _dataRoot));
 
-  final port = args.isNotEmpty ? (int.tryParse(args[0]) ?? _defaultPort) : _defaultPort;
+  final port = p == null ? _defaultPort : (int.tryParse(p) ?? _defaultPort);
   io.serve(handler, '0.0.0.0', port).then((server) {
     print('start server at ${server.address.host}:${server.port} ...');
   });
