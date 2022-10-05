f23229097248a2dabda4c6bd6551ebe9c52b30ee
lindeer <le.chang118@gmail.com>
2022-10-05 20:56:46 +0800

    serve web

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -2,11 +2,12 @@
 import 'package:cloudoc_server/api.dart' as api;
 import 'package:cloudoc_server/src/serve_context.dart';
 import 'package:cloudoc_server/view.dart' as view;
-import 'package:shelf/shelf.dart' show Pipeline, logRequests;
+import 'package:shelf/shelf.dart' show Cascade, Pipeline, logRequests;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
+import 'package:shelf_static/shelf_static.dart' show createStaticHandler;
 
-const _dataRoot = '_data';
+const _dataRoot = '_root';
 const _defaultPort = 8989;
 
 void main(List<String> args) {
@@ -26,18 +27,28 @@ void main(List<String> args) {
     }
   }
 
+  dir ??= _dataRoot;
   final context = ServeContext(
-    root: dir ?? _dataRoot,
+    root: '$dir/data',
     docServer: 'http://192.168.0.130',
   );
 
   api.serve(context);
   view.serve(context);
 
+  final webHandler = createStaticHandler('$dir/web',
+    defaultDocument: 'index.html',
+  );
+
+  final cascade = Cascade()
+      .add(webHandler)
+      .add(context.router);
+
   final handler = const Pipeline()
       .addMiddleware(logRequests())
       .addMiddleware(corsHeaders())
-      .addHandler(context.router);
+      .addHandler(cascade.handler);
+
 
   final port = p == null ? _defaultPort : (int.tryParse(p) ?? _defaultPort);
   io.serve(handler, '0.0.0.0', port).then((server) {
